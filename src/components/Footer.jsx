65008753a0bc2a828c4c95848528f75e7fd5159f
MeganeFarelle <m.demgne@alustudent.com>
2025-12-03 13:54:09 +0100

    Initial project with React Dark Mode using Context API

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import ChangeMode from "./ChangeMode.jsx";
+
+export default function Footer() {
+  return (
+    <footer className="h-[20vh] flex items-center justify-center gap-4 border-t">
+      <p>Footer</p>
+      <ChangeMode />
+    </footer>
+  );
+}
